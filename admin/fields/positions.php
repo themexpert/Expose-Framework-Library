af188c0495033153f2bfeba32de0f5b658846f9c
Parvez <everexpert@gmail.com>
2011-11-18 01:41:52 +0800

    changed positions to module positions field

--- admin/fields/positions.php
+++ admin/fields/positions.php
@@ -27,30 +27,6 @@ class JFormFieldPositions extends JFormField
 	protected $type = 'Positions';
 
 	protected function getInput() {
-        if(!defined('EXPOSE_MENU_PARAMS')){
-            define('EXPOSE_MENU_PARAMS',1);
-            $uri = str_replace(DS,"/",str_replace( JPATH_SITE, JURI::base (), dirname(__FILE__) ));
-			$uri = str_replace("/administrator/", "", $uri);
-            $jquery_uri = str_replace('admin/fields','interface/js',$uri) ;
-            $uri = str_replace("fields",'widgets',$uri);
-
-            JHtml::script($jquery_uri.'/jquery-1.5.1.min.js');
-            JHtml::script($uri.'/menu.params.js');
-        }
-        
-		$db =& JFactory::getDBO();
-		$query = "SELECT DISTINCT position FROM #__modules ORDER BY position ASC";
-		$db->setQuery($query);
-		$groups = $db->loadObjectList();
-		
-		$groupHTML = array();	
-		if ($groups && count ($groups)) {
-			foreach ($groups as $v=>$t){
-				$groupHTML[] = JHTML::_('select.option', $t->position, $t->position);
-			}
-		}
-		$lists = JHTML::_('select.genericlist', $groupHTML, $this->name.'[]', ' multiple="multiple"  size="10" ', 'value', 'text', $this->value);
-		
-		return $lists;
+       
 	}
 } 
