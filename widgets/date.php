027825548157e8ab5ccb57aae07ad25b147ad28c
Parvez <everexpert@gmail.com>
2011-12-03 05:54:41 +0800

    refactored date widget

--- widgets/date.php
+++ widgets/date.php
@@ -5,8 +5,6 @@
  * @author      ThemeXpert http://www.themexpert.com
  * @copyright   Copyright (C) 2010 - 2011 ThemeXpert
  * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3
- * @filesource
- * @file        date.php
  **/
 //prevent direct access
 defined ('EXPOSE_VERSION') or die ('resticted aceess');
@@ -20,20 +18,17 @@ class ExposeWidgetDate extends ExposeWidget{
 
     public function render()
     {
-        return 'date';
+        $now = &JFactory::getDate();
+        $formate = $this->get('formats');
+
+        ob_start();
+        ?>
+        <div id="ex-date">
+            <span><?php echo $now->toFormat($formate) ;?></span>
+        </div>
+    <?php
+        echo ob_get_clean();
     }
 }
-/*
-if($this->get('date')){
-    $now = &JFactory::getDate();
-    $formate = $this->get('formats');
-    ob_start();
-    ?>
-    <div id="tx-date">
-        <span><?php echo $now->toFormat($formate) ;?></span>
-    </div>
-<?php
-echo ob_get_clean();
-}*/
 
 ?>
