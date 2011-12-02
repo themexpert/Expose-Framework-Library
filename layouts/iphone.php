4f668c2f9388ba7edbe1c97fec5a395996a15394
Parvez <everexpert@gmail.com>
2011-12-03 05:09:50 +0800

    updated render body function to work with desktop and mobile platform

--- layouts/iphone.php
+++ layouts/iphone.php
@@ -13,39 +13,6 @@
 //prevent direct access
 defined ('EXPOSE_VERSION') or die ('resticted aceess');
 
-$display_component = !($expose->isHomePage() AND $expose->get('component_display')==FALSE );
+echo 'iphone layout loaded' ;
+
 ?>
-        <?php $expose->loadGists('mobile-logo');?>
-
-        <?php /**Begin Mobile-Header**/ if($expose->countModules('mobile-header',0)): ?>
-        <!--Start Mobile Header Modules-->
-        <div id="tx-mobile-header">
-            <div class="tx-container clearfix">
-                <?php $expose->renderModules('mobile-header','standard',array(/*pass the class name seperate by , */)); ?>
-                <div class="clear"></div>
-            </div>
-        </div>
-        <!--End Mobile Header Modules-->
-        <?php /**End Mobile-Header**/ endif;?>
-
-        <div class="tx-main-inner">
-            <div id="tx-mainbody" class="iphone">
-                <div class="tx-content">
-                    <jdoc:include type="message" />
-                    <?php if($display_component) :?>
-                        <jdoc:include type="component" />
-                    <?php endif;?>
-                </div>
-            </div>
-        </div>
-        
-        <?php /**Begin Mobile Footer**/ if($expose->countModules('mobile-footer',0)): ?>
-        <!--Start Mobile Footer Modules-->
-        <div id="tx-mobile-footer">
-            <div class="tx-container">
-                <?php $expose->renderModules('mobile-footer','standard',array(/*pass the class name seperate by , */)); ?>
-                <div class="clear"></div>
-            </div>
-        </div>
-        <!--End Mobile Footer Modules-->
-        <?php /**End Mobile Footer**/ endif;?>
