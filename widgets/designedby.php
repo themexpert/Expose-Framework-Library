cb67adb047695cc667271e0e9839405cd29968a8
Parvez <everexpert@gmail.com>
2011-12-03 06:11:27 +0800

    refactored legal to designedby widget

--- widgets/designedby.php
+++ widgets/designedby.php
@@ -5,8 +5,6 @@
  * @author      ThemeXpert http://www.themexpert.com
  * @copyright   Copyright (C) 2010 - 2011 ThemeXpert
  * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3
- * @filesource
- * @file        legal.php
  **/
 
 //prevent direct access
@@ -15,25 +13,24 @@ defined ('EXPOSE_VERSION') or die ('resticted aceess');
 //import parent gist class
 expose_import('core.widget');
 
-class ExposeWidgetLegal extends ExposeWidget{
+class ExposeWidgetDesignedby extends ExposeWidget{
     
-    public $name = 'legal';
+    public $name = 'designedby';
 
     public function render()
     {
 
+        ob_start()
+        ?>
+        <div class="designed-by">
+            <p>Designed by <a target="_blank" title="ThemeXpert" href="http://www.themexpert.com">ThemeXpert</a></p>
+        </div>
+
+
+        <?php
+        return ob_get_clean();
     }
 }
-/*
-ob_start()
-?>
-<div class="designed-by">
-    <p>Designed by <a target="_blank" title="ThemeXpert" href="http://www.themexpert.com">ThemeXpert</a></p>
-</div>
-
-
-<?php
-echo ob_get_clean(); */
 
 ?>
 
