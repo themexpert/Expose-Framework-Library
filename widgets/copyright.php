558180c520841d0ab85988d5995da84541d698fd
Parvez <everexpert@gmail.com>
2011-12-03 05:52:40 +0800

    refactored copyright widget

--- widgets/copyright.php
+++ widgets/copyright.php
@@ -5,8 +5,6 @@
  * @author      ThemeXpert http://www.themexpert.com
  * @copyright   Copyright (C) 2010 - 2011 ThemeXpert
  * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3
- * @filesource
- * @file        copyright.php
  **/
 //prevent direct access
 defined ('EXPOSE_VERSION') or die ('resticted aceess');
@@ -20,20 +18,15 @@ class ExposeWidgetCopyright extends ExposeWidget{
 
     public function render()
     {
-        return 'copyright gist';
+        ob_start();
+            ?>
+            <div id="ex-copyright">
+                <p>
+                    <?php echo ($this->get('text') ==='EXPOSE_DEFAULT_COPYRIGHT') ? JText::_('EXPOSE_DEFAULT_COPYRIGHT')  : $this->get('text'); ?>
+                </p>
+            </div>
+        <?php
+        echo ob_get_clean();
     }
 }
-/*
-if($this->get('copyright')){
-    ob_start();
-    ?>
-    <div id="tx-copyright">
-        <p>
-            <?php echo ($this->get('copyright_text')==='EXPOSE_DEFAULT_COPYRIGHT') ? JText::_('EXPOSE_DEFAULT_COPYRIGHT')  : $this->get('copyright_text'); ?>
-        </p>
-    </div>
-<?php
-echo ob_get_clean();
-}*/
-
 ?>
