095c0790616d46540678059dc9d305af1fb30446
Parvez <everexpert@gmail.com>
2011-12-04 02:38:13 +0800

    added equal height widget close #10

---
+++ widgets/equalheight.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package     Expose
+ * @version     2.0
+ * @author      ThemeXpert http://www.themexpert.com
+ * @copyright   Copyright (C) 2010 - 2011 ThemeXpert
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3
+ **/
+
+//prevent direct access
+defined ('EXPOSE_VERSION') or die ('resticted aceess');
+
+//import parent gist class
+expose_import('core.widget');
+
+class ExposeWidgetEqualHeight extends ExposeWidget{
+
+    public $name = 'equalheight';
+
+    public function init()
+    {
+        $js = 'equalHeight(jQuery(".ex-box"));';
+        $this->document->addScript($this->exposeUrl.'/interface/js/jquery.equalheight.js');
+        $this->addjQDom($js);
+    }
+}
+
+?>
