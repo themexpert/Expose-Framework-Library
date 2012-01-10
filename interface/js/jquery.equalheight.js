e1fa17a5dc991a17f0f5772ea67c0839a6015392
Parvez <everexpert@gmail.com>
2012-01-10 12:45:01 +0800

    update equal height js

--- interface/js/jquery.equalheight.js
+++ interface/js/jquery.equalheight.js
@@ -1,47 +1,40 @@
-/*-------------------------------------------------------------------- 
- * JQuery Plugin: "EqualHeights" & "EqualWidths"
- * by:	Scott Jehl, Todd Parker, Maggie Costello Wachs (http://www.filamentgroup.com)
- *
- * Copyright (c) 2007 Filament Group
- * Licensed under GPL (http://www.opensource.org/licenses/gpl-license.php)
- *
- * Description: Compares the heights or widths of the top-level children of a provided element 
- 		and sets their min-height to the tallest height (or width to widest width). Sets in em units 
- 		by default if pxToEm() method is available.
- * Dependencies: jQuery library, pxToEm method	(article: http://www.filamentgroup.com/lab/retaining_scalable_interfaces_with_pixel_to_em_conversion/)							  
- * Usage Example: $(element).equalHeights();
-   						      Optional: to set min-height in px, pass a true argument: $(element).equalHeights(true);
- * Version: 2.0, 07.24.2008
- * Changelog:
- *  08.02.2007 initial Version 1.0
- *  07.24.2008 v 2.0 - added support for widths
---------------------------------------------------------------------*/
+(function($) {
+var currentTallest = 0,
+     currentRowStart = 0,
+     rowDivs = new Array(),
+     $el,
+     topPosition = 0;
 
-$.fn.equalHeights = function(px) {
-	$(this).each(function(){
-		var currentTallest = 0;
-		$(this).children().each(function(i){
-			if ($(this).height() > currentTallest) { currentTallest = $(this).height(); }
-		});
-		if (!px || !Number.prototype.pxToEm) currentTallest = currentTallest.pxToEm(); //use ems unless px is specified
-		// for ie6, set height since min-height isn't supported
-		if ($.browser.msie && $.browser.version == 6.0) { $(this).children().css({'height': currentTallest}); }
-		$(this).children().css({'min-height': currentTallest}); 
-	});
-	return this;
-};
+ $('#tx-roof > div').each(function() {
 
-// just in case you need it...
-$.fn.equalWidths = function(px) {
-	$(this).each(function(){
-		var currentWidest = 0;
-		$(this).children().each(function(i){
-				if($(this).width() > currentWidest) { currentWidest = $(this).width(); }
-		});
-		if(!px || !Number.prototype.pxToEm) currentWidest = currentWidest.pxToEm(); //use ems unless px is specified
-		// for ie6, set width since min-width isn't supported
-		if ($.browser.msie && $.browser.version == 6.0) { $(this).children().css({'width': currentWidest}); }
-		$(this).children().css({'min-width': currentWidest}); 
-	});
-	return this;
-};
+   $el = $(this);
+   topPostion = $el.position().top;
+
+   if (currentRowStart != topPostion) {
+
+     // we just came to a new row.  Set all the heights on the completed row
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+
+     // set the variables for the new row
+     rowDivs.length = 0; // empty the array
+     currentRowStart = topPostion;
+     currentTallest = $el.height();
+     rowDivs.push($el);
+
+   } else {
+
+     // another div on the current row.  Add it to the list and check if it's taller
+     rowDivs.push($el);
+     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+
+  }
+
+  // do the last row
+   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+     rowDivs[currentDiv].height(currentTallest);
+   }
+
+ });
+})(jQuery);
