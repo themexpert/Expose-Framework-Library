59a6c622348d39913e5e8086b65f522ca9da8443
Parvez <everexpert@gmail.com>
2012-01-11 20:19:36 +0800

    xhtml code added

--- layouts/sidebara-sidebarb-content.php
+++ layouts/sidebara-sidebarb-content.php
@@ -1,2 +1,71 @@
 <?php
-echo 'sidebara-sidebarb-contetn loaded';
+/**
+ * @package     Expose
+ * @version     3.0.0
+ * @author      ThemeXpert http://www.themexpert.com
+ * @copyright   Copyright (C) 2010 - 2011 ThemeXpert
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3
+ **/
+
+//prevent direct access
+defined ('EXPOSE_VERSION') or die ('resticted aceess');
+
+    global $expose;
+    $width = $expose->getComponentWidth();
+?>
+
+<div id="ex-main" class="ex-row">
+
+    <jdoc:include type="message" />
+
+    <div id="ex-mainbody" class="ex-column" style="width:<?= $width['component']; ?>%;left: <?= $width['sidebar-a'] + $width['sidebar-b'] ; ?>%">
+
+        <?php /**Begin Content top**/ if($expose->countModules('contenttop')): ?>
+        <!--Start Content Top Modules-->
+        <div id="ex-contenttop">
+            <?php $expose->renderModules('contenttop'); ?>
+            <div class="clear"></div>
+        </div>
+        <!--End Content top Modules-->
+        <?php /**End Content top **/ endif;?>
+
+        <div id="ex-content" role="article">
+            <div class="ex-container">
+                <div class="ex-block">
+                    <jdoc:include type="component" />
+                </div>
+            </div>
+        </div>
+
+        <?php /**Begin Content bottom**/ if($expose->countModules('contentbottom')): ?>
+        <!--Start Content Bottom Modules-->
+        <div id="ex-contentbottom">
+            <?php $expose->renderModules('contentbottom'); ?>
+            <div class="clear"></div>
+        </div>
+        <!--End Content Bottom Modules-->
+        <?php /**End Content bottom **/ endif;?>
+
+    </div>
+
+    <?php /**Begin Sidebar-A**/ if($expose->countModules('sidebar-a')): ?>
+
+    <!--Start Sidebar-A Modules-->
+    <div id="ex-sidebar-a" class="ex-column" role="complementary" style="width:<?= $expose->getSidebarsWidth('sidebar-a') ?>%; left: -<?= $width['component'] ?>%">
+        <?php $expose->renderModules('sidebar-a'); ?>
+        <div class="clear"></div>
+    </div>
+
+    <!--End Sidebar-A Modules-->
+    <?php /**End Sidebar-A **/ endif;?>
+
+    <?php /**Begin Sidebar-B**/ if($expose->countModules('sidebar-b')): ?>
+
+        <!--Start Sidebar-B Modules-->
+        <div id="ex-sidebar-b" class="ex-column" role="complementary" style="width:<?= $expose->getSidebarsWidth('sidebar-b') ?>%;left: -<?= $width['component'] ?>%">
+            <?php $expose->renderModules('sidebar-b'); ?>
+            <div class="clear"></div>
+        </div>
+        <!--End Sidebar-B Modules-->
+    <?php /**End Sidebar-B **/ endif;?>
+</div>
