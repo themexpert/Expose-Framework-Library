85aeb72f2275bad989ff8819ef918555d5db4a45
Parvez <everexpert@gmail.com>
2012-01-15 03:33:42 +0800

    added new mobile layout, close #7

--- layouts/iphone.php
+++ layouts/iphone.php
@@ -1,18 +1,55 @@
 <?php
 /**
- * Description of iphone
- *
  * @package     Expose
- * @version     2.0    Mar 23, 2011
+ * @version     3.0.0
  * @author      ThemeXpert http://www.themexpert.com
  * @copyright   Copyright (C) 2010 - 2011 ThemeXpert
  * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3
- * @filesource
- * @file        iphone.php
  **/
+
 //prevent direct access
 defined ('EXPOSE_VERSION') or die ('resticted aceess');
 
-echo 'iphone layout loaded' ;
+global $expose;
 
 ?>
+
+<div id="ex-main" class="ex-row">
+
+    <jdoc:include type="message" />
+
+    <div id="ex-mainbody" class="ex-column">
+
+        <?php /**Begin Content top**/ if($expose->countModules('contenttop')): ?>
+        <!--Start Content Top Modules-->
+        <div id="ex-contenttop">
+            <?php $expose->renderModules('contenttop'); ?>
+            <div class="clear"></div>
+        </div>
+        <!--End Content top Modules-->
+        <?php /**End Content top **/ endif;?>
+
+        <?php if($expose->displayComponent()): ?>
+        <div id="ex-content" role="article">
+            <div class="ex-container">
+                <div class="ex-block">
+                    <jdoc:include type="component" />
+                </div>
+            </div>
+        </div>
+        <?php endif;?>
+
+        <?php /**Begin Content bottom**/ if($expose->countModules('contentbottom')): ?>
+        <!--Start Content Bottom Modules-->
+        <div id="ex-contentbottom">
+            <?php $expose->renderModules('contentbottom'); ?>
+            <div class="clear"></div>
+        </div>
+        <!--End Content Bottom Modules-->
+        <?php /**End Content bottom **/ endif;?>
+
+    </div>
+
+    <div class="clear"></div>
+
+</div>
