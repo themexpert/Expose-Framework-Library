046504e3998efe432a0ad2318c9fe46111bbdecc
Parvez <everexpert@gmail.com>
2012-02-11 15:49:05 +0800

    code cleanup

--- core/typography.php
+++ core/typography.php
@@ -7,8 +7,7 @@
  * @author      ThemeXpert http://www.themexpert.com
  * @copyright   Copyright (C) 2010 - 2011 ThemeXpert
  * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPLv3
- * @filesource
- * @file        typography.php
+ *
  **/
 //prevent direct access
 defined ('EXPOSE_VERSION') or die ('resticted aceess');
