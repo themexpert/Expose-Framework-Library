9f2144e1b7a2d962c7e98d0f865b29b683add08c
Parvez <parvez@themexpert.com>
2012-08-14 04:54:55 +0800

    Admin panel converted to grid

--- admin/fields/positionlayouts.php
+++ admin/fields/positionlayouts.php
@@ -23,19 +23,49 @@ class JFormFieldPositionLayouts extends JFormField{
 
         $maxmods = (int) $this->element['max-mods'];
         $html = '';
+        $grid = 12;
 
-        $id = str_replace('jform_params_','',$this->id);
-        $finalValue = array();
+        $id                 = str_replace('jform_params_','',$this->id);
+        $finalValue         = array();
+        $tempDefaultVaule   = array();
 
         if(!empty($this->value)){
-            $tempVal = explode(',',$this->value);
+            $tempVal        = explode(',',$this->value);
 
             foreach($tempVal as $value){
                 list($index, $val) = explode(':',$value);
                 $finalValue[$index][] = $val;
             }
-        }
 
+            /*
+             * Expose 3.0 backward compatibility
+             * This code will check and compare with default value, if find old % based value replace with
+             * grid value
+             *
+             * @deprecated : 4.0
+             * */
+            $tempDefault    = explode(',',$this->element['default']);
+
+            foreach($tempDefault as $value){
+                list($index, $val) = explode(':',$value);
+                $tempDefaultVaule[$index][] = $val;
+            }
+
+            //compare with default value with old value
+            foreach( $tempDefaultVaule as $index => $val )
+            {
+                if( is_array( $val ) ){
+                    foreach( $val as $key => $v )
+                    {
+
+                        if( $finalValue[$index][$key] > 12 OR !isset($finalValue[$index][$key]) ){
+
+                            $finalValue[$index][$key] = $tempDefaultVaule[$index][$key];
+                        }
+                    }
+                }
+            }
+        }
 
         $html .= "<div id='mods-wrap' data-mods='".$maxmods."'>";
             $html .= "<div class='mod-tabs'>";
@@ -49,13 +79,14 @@ class JFormFieldPositionLayouts extends JFormField{
             for($pan=1; $pan<=$maxmods; $pan++){
                 $html .= "<div class='inputs'>";
                     for($input=0; $input<$pan; $input++){
+
                         if(empty($this->value)){
-                            $val = 100/$pan;
-                            if(is_float($val)) $val = sprintf("%01.2f", 100/$pan);
+                            $val = $grid/$pan;
+                            //if(is_float($val)) $val = sprintf("%01.2f", 100/$pan);
                             $finalValue[$pan][$input] = $val;
                         }
                         //echo $finalValue[1][1] , ',';
-                        $html .= "<input type='text' class='inputbox' value='".($finalValue[$pan][$input])."' /><span>%</span>";
+                        $html .= "<input type='text' class='inputbox' value='".($finalValue[$pan][$input])."' />";
                     }
                 $html .= "</div>";
             }
@@ -67,6 +98,17 @@ class JFormFieldPositionLayouts extends JFormField{
         $html .= "<input type='hidden' name='".$this->name."' id='".$this->id."' value='".$this->value."' />";
 
         $js = "
+        // Expose 3.0 Backward compatibility code
+        var o = jQuery('#jform_params_$id').val(),
+            n = o.split(','),
+            f = n[0].split(':'),
+            g = '1:12,2:6,2:6,3:4,3:4,3:4,4:3,4:3,4:3,4:3,5:3,5:2,5:2,5:2,5:3,6:2,6:2,6:2,6:2,6:2,6:2';
+        //assuming that u have old value
+        if( f[1] >= 100 )
+        {
+           jQuery('#jform_params_$id').val(g);
+        }
+
         jQuery('#$id .inputbox').bind('keyup',function(){
         var valx = '';
         jQuery('#$id .inputs').each(function(index1,value1){
@@ -74,7 +116,7 @@ class JFormFieldPositionLayouts extends JFormField{
                  valx += (index1 + 1) + ':' +  jQuery(this).val() + ',' ;
             });
         });
-        valx = valx.slice(0,-1);//trim , from end
+        valx = valx.slice(0,-1);//trim [,] from end
         jQuery('#jform_params_$id').val(valx);
     });";
         $expose->addjQDom($js);
