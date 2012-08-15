efbeeabdb67a4670c8e27869ba0244ffbeaa7ca5
Parvez <parvez@themexpert.com>
2012-08-15 09:11:45 +0800

    updated layout to grid

--- layouts/sidebara-content-sidebarb.php
+++ layouts/sidebara-content-sidebarb.php
@@ -11,37 +11,36 @@
 defined ('EXPOSE_VERSION') or die ('resticted aceess');
 
     global $expose;
-    $width = $expose->getComponentWidth();
-    $prefix = $expose->getPrefix();
-    $leftright = ($expose->direction == 'rtl') ? 'right' : 'left';
+    $grid = $expose->getComponentWidth();
 ?>
-<div id="<?php echo $prefix;?>main" class="<?php echo $prefix;?>row">
+<div id="main" class="container">
 
+    <div class="row">
     <jdoc:include type="message" />
 
-    <div id="<?php echo $prefix;?>mainbody" class="ex-column" style="width:<?php echo $width['component']; ?>%;<?php echo $leftright; ?>: <?php echo $width['sidebar-a']; ?>%">
+    <div id="mainbody" class="grid<?php echo $grid['component'];?> offset<?php echo $grid['sidebar-a']; ?>">
 
         <?php /**Begin Content top**/ if($expose->countModules('contenttop')): ?>
         <!--Start Content Top Modules-->
-        <div id="<?php echo $prefix;?>content-top" class="clearfix">
+        <div id="content-top" class="row">
             <?php $expose->renderModules('contenttop'); ?>
         </div>
         <!--End Content top Modules-->
         <?php /**End Content top **/ endif;?>
 
         <?php if($expose->displayComponent()): ?>
-        <div id="ex-component" role="article" class="clearfix">
-            <div class="ex-container">
-                <div class="ex-block">
-                    <jdoc:include type="component" />
-                </div>
+        <div id="component" role="article" class="row">
+
+            <div class="block">
+                <jdoc:include type="component" />
             </div>
+
         </div>
         <?php endif;?>
 
         <?php /**Begin Content bottom**/ if($expose->countModules('contentbottom')): ?>
         <!--Start Content Bottom Modules-->
-        <div id="<?php echo $prefix;?>content-bottom" class="clearfix">
+        <div id="content-bottom" class="clearfix">
             <?php $expose->renderModules('contentbottom'); ?>
         </div>
         <!--End Content Bottom Modules-->
@@ -52,9 +51,8 @@ defined ('EXPOSE_VERSION') or die ('resticted aceess');
     <?php /**Begin Sidebar-A**/ if($expose->countModules('sidebar-a')): ?>
 
     <!--Start Sidebar-A Modules-->
-    <div id="<?php echo $prefix;?>sidebar-a" class="ex-column clearfix" role="complementary" style="width:<?php echo $expose->getSidebarsWidth('sidebar-a') ?>%; <?php echo $leftright; ?>: -<?php echo $width['component'] ?>%">
+    <div id="sidebar-a" class="grid<?php echo $grid['sidebar-a'];?> inset<?php echo ($grid['component'] + $grid['sidebar-a']) ?>" role="complementary">
         <?php $expose->renderModules('sidebar-a'); ?>
-
     </div>
 
     <!--End Sidebar-A Modules-->
@@ -63,13 +61,10 @@ defined ('EXPOSE_VERSION') or die ('resticted aceess');
     <?php /**Begin Sidebar-B**/ if($expose->countModules('sidebar-b')): ?>
 
         <!--Start Sidebar-B Modules-->
-        <div id="<?php echo $prefix;?>sidebar-b" class="ex-column clearfix" role="complementary" style="width:<?php echo $expose->getSidebarsWidth('sidebar-b') ?>%">
+        <div id="sidebar-b" class="grid<?php echo $expose->getSidebarsWidth('sidebar-b') ?>" role="complementary">
             <?php $expose->renderModules('sidebar-b'); ?>
-
         </div>
         <!--End Sidebar-B Modules-->
     <?php /**End Sidebar-B **/ endif;?>
-
-
-
+    </div>
 </div>
