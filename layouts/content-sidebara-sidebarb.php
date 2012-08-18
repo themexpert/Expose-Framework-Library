285f5e72401e170617d7a29e0716fb545afcec2a
Parvez <parvez@themexpert.com>
2012-08-19 05:07:08 +0800

    Fixed Issue #39 - Implemented HTML5 markup

--- layouts/content-sidebara-sidebarb.php
+++ layouts/content-sidebara-sidebarb.php
@@ -13,58 +13,50 @@ defined ('EXPOSE_VERSION') or die ('resticted aceess');
     global $expose;
     $grid = $expose->getComponentWidth();
 ?>
-<div id="main" class="container">
+<jdoc:include type="message" />
 
-    <div class="row">
-    <jdoc:include type="message" />
+<section id="mainbody" role="main" class="grid<?php echo $grid['component'];?>">
 
-    <div id="mainbody" class="grid<?php echo $grid['component'];?>">
+    <?php /**Begin Content top**/ if($expose->countModules('contenttop')): ?>
+    <!--Start Content Top Modules-->
+    <section id="content-top" class="clearfix">
+        <?php $expose->renderModules('contenttop', TRUE); ?>
+    </section>
+    <!--End Content top Modules-->
+    <?php /**End Content top **/ endif;?>
 
-        <?php /**Begin Content top**/ if($expose->countModules('contenttop')): ?>
-        <!--Start Content Top Modules-->
-        <div id="content-top" class="row">
-            <?php $expose->renderModules('contenttop', TRUE); ?>
+    <?php if($expose->displayComponent()): ?>
+    <section id="component" role="article" class="clearfix">
+
+        <div class="block">
+            <jdoc:include type="component" />
         </div>
-        <!--End Content top Modules-->
-        <?php /**End Content top **/ endif;?>
 
-        <?php if($expose->displayComponent()): ?>
-        <div id="component" role="article" class="row">
+    </section>
+    <?php endif;?>
 
-            <div class="block">
-                <jdoc:include type="component" />
-            </div>
+    <?php /**Begin Content bottom**/ if($expose->countModules('contentbottom')): ?>
+    <!--Start Content Bottom Modules-->
+    <section id="content-bottom" class="clearfix">
+        <?php $expose->renderModules('contentbottom', TRUE); ?>
+    </section>
+    <!--End Content Bottom Modules-->
+    <?php /**End Content bottom **/ endif;?>
 
-        </div>
-        <?php endif;?>
+</section>
 
-        <?php /**Begin Content bottom**/ if($expose->countModules('contentbottom')): ?>
-        <!--Start Content Bottom Modules-->
-        <div id="content-bottom" class="row">
-            <?php $expose->renderModules('contentbottom', TRUE); ?>
-        </div>
-        <!--End Content Bottom Modules-->
-        <?php /**End Content bottom **/ endif;?>
-
-    </div>
-
-    <?php /**Begin Sidebar-A**/ if($expose->countModules('sidebar-a')): ?>
-
+<?php /**Begin Sidebar-A**/ if($expose->countModules('sidebar-a')): ?>
     <!--Start Sidebar-A Modules-->
-    <div id="sidebar-a" class="grid<?php echo $grid['sidebar-a'];?>" role="complementary">
+    <aside id="sidebar-a" class="grid<?php echo $grid['sidebar-a'];?>" role="complementary">
         <?php $expose->renderModules('sidebar-a'); ?>
-    </div>
-
+    </aside>
     <!--End Sidebar-A Modules-->
-    <?php /**End Sidebar-A **/ endif;?>
+<?php /**End Sidebar-A **/ endif;?>
 
-    <?php /**Begin Sidebar-B**/ if($expose->countModules('sidebar-b')): ?>
-
-        <!--Start Sidebar-B Modules-->
-        <div id="sidebar-b" class="grid<?php echo $grid['sidebar-b'] ?>" role="complementary">
-            <?php $expose->renderModules('sidebar-b'); ?>
-        </div>
-        <!--End Sidebar-B Modules-->
-    <?php /**End Sidebar-B **/ endif;?>
-    </div>
-</div>
+<?php /**Begin Sidebar-B**/ if($expose->countModules('sidebar-b')): ?>
+    <!--Start Sidebar-B Modules-->
+    <aside id="sidebar-b" class="grid<?php echo $grid['sidebar-b'] ?>" role="complementary">
+        <?php $expose->renderModules('sidebar-b'); ?>
+    </aside>
+    <!--End Sidebar-B Modules-->
+<?php /**End Sidebar-B **/ endif;?>
